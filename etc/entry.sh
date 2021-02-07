defa0ec715e263ada92df66d519ae9cceedb2158
CM2Walki <walentinlamonos@googlemail.com>
2021-02-07 12:47:39 +0100

    Added missing files

---
+++ etc/entry.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+mkdir -p "${STEAMAPPDIR}" || true  
+
+# Override SteamCMD launch arguments if necessary
+# Used for subscribing to betas or for testing
+if [ -z "$STEAMCMD_UPDATE_ARGS" ]; then
+	bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous +force_install_dir "$STEAMAPPDIR" +app_update "$STEAMAPPID" +quit
+else
+	steamcmd_update_args=($STEAMCMD_UPDATE_ARGS)
+	bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous +force_install_dir "$STEAMAPPDIR" +app_update "$STEAMAPPID" "${steamcmd_update_args[@]}" +quit
+fi
+
+# We assume that if the config is missing, that this is a fresh container
+if [ ! -f "${STEAMAPPDIR}/${STEAMAPP}/cfg/Game.ini" ]; then
+	# Create directory
+	mkdir -p "${STEAMAPPDIR}/${STEAMAPP}/"
+	
+	# Download & extract the configs
+	wget -qO- "${DLURL}/master/etc/Config.tar.gz" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}/"
+	
+	# Change first launch variables (you can comment this out if it has done it's purpose)
+	sed -i -e 's/{{SERVER_PW}}/'"${SERVER_PW}"'/g' \
+			-e 's/{{SERVER_ADMINPW}}/'"${SERVER_ADMINPW}"'/g' \
+			-e 's/{{SERVER_NAME}}/'"${SERVER_NAME}"'/g' \
+			-e 's/{{SERVER_MAXPLAYERS}}/'"${SERVER_MAXPLAYERS}"'/g' "${STEAMAPPDIR}/${STEAMAPP}/Saved/Config/LinuxServer/Game.ini"
+
+	sed -i -e 's/{{SERVER_TICKRATE}}/'"${SERVER_TICKRATE}"'/g' \
+			-e 's/{{SERVER_DEFAULTMAP}}/'"${SERVER_DEFAULTMAP}"'/g' "${STEAMAPPDIR}/${STEAMAPP}/Saved/Config/LinuxServer/Engine.ini"
+fi
+
+# Switch to workdir
+cd ${STEAMAPPDIR}
+
+bash "${STEAMAPPDIR}/MordhauServer.sh" \
+			-log \
+			-Port="${SERVER_PORT}" \
+			-QueryPort="${SERVER_QUERYPORT}" \
+			-BeaconPort="${SERVER_BEACONPORT}" \
+			-GAMEINI="${STEAMAPPDIR}/${STEAMAPP}/${SERVER_GAMEINI}" \
+			-ENGINEINI="${STEAMAPPDIR}/${STEAMAPP}/${SERVER_ENGINEINI}" \
+			"${ADDITIONAL_ARGS}"
